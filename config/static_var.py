43895bcd0099814260fb76ef5cd4f64bee7449ce
Ludovic Ortega <mastership@hotmail.fr>
2019-04-07 15:27:00 +0300

    -cache +vertigo
    You can still change map pool by editing the list named "mapool_csgo" in config/static_var.py

--- config/static_var.py
+++ config/static_var.py
@@ -78,4 +78,4 @@ ebot_status = ["not started", "starting", "warmup knife round", "knife round", "
                "warmup 2nd side OT", "second side OT", "finished", "archived"]
 
 # Active Duty Map Pool csgo
-mapool_csgo = ["de_dust2", "de_cache", "de_inferno", "de_train", "de_overpass", "de_mirage", "de_nuke"]
+mapool_csgo = ["de_dust2", "de_vertigo", "de_inferno", "de_train", "de_overpass", "de_mirage", "de_nuke"]
